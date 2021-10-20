952cdf8c757ca7bccdb8594005c3ab872097a2ee
mattcmerritt <mattcmerritt@gmail.com>
2021-10-20 02:51:31 -0400

    added simple inventory display with boxes that had be hidden

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,12 +5,23 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    // warning display data
     public GameObject WarningHUD;
     public bool Detected;
+
+    // inventory data
+    public GameObject Inventory;
+    public bool InventoryActive;
 
     public void Update()
     {
         WarningHUD.SetActive(Detected);
+
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleInventory();
+        }
     }
 
     public void PlayerDetected()
@@ -21,5 +32,11 @@ public class UI : MonoBehaviour
     public void PlayerLost()
     {
         Detected = false;
+    }
+
+    public void ToggleInventory()
+    {
+        InventoryActive = !InventoryActive;
+        Inventory.SetActive(InventoryActive);
     }
 }
