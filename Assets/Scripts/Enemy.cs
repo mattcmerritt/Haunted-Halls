b22282064ac2a9a4c1cdec58c694f40c3030015f
mattcmerritt <mattcmerritt@gmail.com>
2021-10-24 19:32:44 -0400

    added nodes that check when the enemy crosses them

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MonoBehaviour
 
     public UI UI;
 
+    public GameObject NavigationMap;
+
     public void Awake()
     {
         UI = FindObjectOfType<UI>();
@@ -16,6 +18,7 @@ public class Enemy : MonoBehaviour
 
     public void Update()
     {
+        // looking for player
         if (Physics.Raycast(transform.position, transform.forward, VisionRange))
         {
             UI.PlayerDetected();
@@ -23,6 +26,15 @@ public class Enemy : MonoBehaviour
         else
         {
             UI.PlayerLost();
+        }
+
+        // moving between nodes
+        PathingNode activeNode = NavigationMap.GetComponentInChildren<PathingNode>();
+        
+        if (activeNode != null)
+        {
+            Vector3 direction = Vector3.Normalize(activeNode.gameObject.transform.position - transform.position);
+            Debug.DrawLine(transform.position, transform.position + (direction * 10f), Color.magenta);
         }
     }
 
